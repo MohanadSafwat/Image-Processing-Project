9bbcc3559d90567e4cd23d02901abbdf6f11bf68
Mohanad Safwat <57914175+MohanadSafwat@users.noreply.github.com>
2022-05-23 13:57:41 +0200

    Add HOG

---
+++ Phase2/features.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+from skimage.feature import hog
+import matplotlib.pyplot as plt
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, 
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), 
+                                  transform_sqrt=True, 
+                                  visualize=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, 
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), 
+                       transform_sqrt=True, 
+                       visualize=vis, feature_vector=feature_vec)
+        return features
+
